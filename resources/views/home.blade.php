048690989c1178a0ad578b97272319f287b371c0
Victor Ramirez <victor.ramirez@academicos.udg.mx>
2022-02-14 09:47:51 -0600

    Cambio de ciclo en cursos en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,7 @@
                             @endif
                             <br>
                             <div class="row align-items-center">
-                                <div class="col-md-2 offset-md-1">
+                                <div class="col-md-3 offset-md-1 text-end">
                                     <h3 class="card-title"> <span class="text-success"><i class="fa fa-search"></span></i> Búsqueda</3>
                                 </div>
                                 <div class="col-md-5">
@@ -63,7 +63,7 @@
 <div class="row m-1">
 
     @if (Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='cta' || Auth::user()->role =='auxiliar' || Auth::user()->role =='redes' ))
-       <div class="col-lg-4 col-md-6 col-sm-6">
+       <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats ">
             <div class="card-header card-header-warning card-header-icon">
             <div class="card-icon">
@@ -72,8 +72,8 @@
            
             <h3 class="card-title">Equipos <br></h3>
                 <a href="{{ route('equipos.create') }}" class="btn btn-outline-success">Capturar Equipo</a>
-                <a href="{{ route('nuevo-prestamo') }}" class="btn btn-outline-success">nuevo prestamo</a>
-		<a href="{{ route('prestamos.index') }}" class="btn btn-outline-warning">Consultar Préstamos</a>
+                <a href="{{ route('nuevo-prestamo') }}" class="btn btn-outline-warning">nuevo prestamo</a>
+		<a href="{{ route('prestamos.index') }}" class="btn btn-outline-danger">Consultar Préstamos</a>
             </div>
             <div class="card-footer">
             <div class="stats">
@@ -106,7 +106,7 @@
     @endif 
 <!-- Apartado de Tickets  -->
 	@if(Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='cta' || Auth::user()->role =='auxiliar' || Auth::user()->role =='redes'))
-	<div class="col-lg-4 col-md-6 col-sm-6">
+	<div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-danger card-header-icon">
             <div class="card-icon">
@@ -128,7 +128,7 @@
 <!-- Apartado de Inventario   -->
 	@if(Auth::check() && (Auth::user()->role =='admin'))
 
-        <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-success card-header-icon">
             <div class="card-icon">
@@ -152,7 +152,7 @@
 <!-- Apartado de Aulas y �reas   -->
 	@if(Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='cta' || Auth::user()->role =='rh' || Auth::user()->role =='redes'))
 
-        <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-secondary card-header-icon">
             	<div class="card-icon">
@@ -160,7 +160,7 @@
            	 </div>
 	         <h3 class="card-title">Aulas y &Aacute;reas</h3>
 	    	<a class="btn btn-outline-success" href="{{ route('areas.index') }}" >Listado &Aacute;reas</a>
-            	<a class="btn btn-outline-secondary" href="{{ route('area-ticket','Belenes') }}" >Detalle Aulas</a>            
+            	<a class="btn btn-outline-danger" href="{{ route('area-ticket','Belenes') }}" >Detalle Aulas</a>            
 	    </div>
             <div class="card-footer">
             <div class="stats">
@@ -173,15 +173,15 @@
 <!-- Apartado de Cursos   -->
 	@if(Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='cta' || Auth::user()->role =='aulas' || Auth::user()->role =='redes' || Auth::user()->role =='auxiliar' || Auth::user()->role == 'general'))
 
-        <div class="col-lg-4 col-md-6 col-sm-6 ">
+        <div class="col-lg-4 col-md-6 col-sm-12 ">
         <div class="card card-stats ">
             <div class="card-header card-header-info card-header-icon">
             <div class="card-icon">
                 <i class="material-icons">school</i>
             </div>
 	    <h3 class="card-title">Cursos</h3>           
-		<a class="btn btn-outline-success" href="{{ route('cursos-presenciales', '2021B') }}">Presenciales</a>            
-		<a class="btn btn-outline-danger" href="{{ route('cursos-laboratorios', '2021B') }}">Laboratorios</a>
+		<a class="btn btn-outline-success" href="{{ route('cursos-presenciales', '2022A') }}">Presenciales</a>
+		<a class="btn btn-outline-danger" href="{{ route('cursos-laboratorios', '2022A') }}">Laboratorios</a>
 	    
             </div>
             <div class="card-footer">
@@ -189,7 +189,7 @@
                 @if(Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='cta' || Auth::user()->role =='aulas' || Auth::user()->role =='redes' || Auth::user()->role =='auxiliar' ))
 		<i class="material-icons">spellcheck</i><a href="{{ route('cursos.create') }}" >Capturar</a>
                 @endif
-                <i class="material-icons">update</i><a  href="{{ url('cursos/2021B') }}">Todos</a>
+                <i class="material-icons">update</i><a  href="{{ url('cursos/2022A') }}">Todos</a>
 
             </div>
             </div>
@@ -198,7 +198,7 @@
 @endif
 <!-- Apartado de Usuarios   -->
 	@if(Auth::check() && (Auth::user()->role =='admin'))
- <div class="col-lg-4 col-md-6 col-sm-6">
+ <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-success card-header-icon">
             <div class="card-icon">
@@ -221,7 +221,7 @@
         
 @endif
     @if (Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='cta' || Auth::user()->role =='auxiliar' || Auth::user()->role =='redes' || Auth::user()->role =='general'))
-       <div class="col-lg-4 col-md-6 col-sm-6 ">
+       <div class="col-lg-4 col-md-6 col-sm-12 ">
         <div class="card card-stats ">
             <div class="card-header card-header-danger card-header-icon">
             <div class="card-icon">
@@ -246,7 +246,7 @@
     @endif 
 
      @if (Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='cta' || Auth::user()->role =='redes'))
-       <div class="col-lg-4 col-md-6 col-sm-6">
+       <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-warning card-header-icon">
             <div class="card-icon">
@@ -268,7 +268,7 @@
     @endif 
 
      @if (Auth::check() && (Auth::user()->role =='admin'))
-       <div class="col-lg-4 col-md-6 col-sm-6">
+       <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-secundary card-header-icon">
             <div class="card-icon">
@@ -293,7 +293,7 @@
 
 
         @if (Auth::check() && (Auth::user()->role =='admin' || Auth::user()->role =='redes'))
-            <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="card card-stats">
                     <div class="card-header card-header-primary card-header-icon">
                         <div class="card-icon">
@@ -317,7 +317,7 @@
 
         @endif
         @if (Auth::check() && (Auth::user()->role =='admin'))
-       <div class="col-lg-4 col-md-6 col-sm-6">
+       <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-success card-header-icon">
             <div class="card-icon">
@@ -338,7 +338,7 @@
 
     @endif 
         @if (Auth::check() && (Auth::user()->role =='admin'))
-       <div class="col-lg-4 col-md-6 col-sm-6">
+       <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card card-stats">
             <div class="card-header card-header-primary card-header-icon">
             <div class="card-icon">
