65bf92ec65899e15d588265df1f7024a801ccb0b
DZavala01 <rexzav1@gmail.com>
2022-02-26 10:23:29 -0600

    Botón de capturar técnico

--- resources/views/tecnicos/index.blade.php
+++ resources/views/tecnicos/index.blade.php
@@ -32,6 +32,7 @@
                 <p align="right">
                     {{-- <a href="{{ route('') }}" class="btn btn-success">Capturar servicio</a> --}}
                     <a href="{{ route('home') }}" class="btn btn-primary">< Regresar</a>
+                    <a href="{{ route('tecnicos.create') }}" class="btn btn-success">< Capturar Técnico</a>
                 </p>
             <table id="example" class="table table-striped table-bordered" style="width:100%">
                 <thead>
