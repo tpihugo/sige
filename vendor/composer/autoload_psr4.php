e99f43c05db2a2d718fae84d834d681434ad1261
Victor Ramirez <victor.ramirez@academicos.udg.mx>
2022-03-12 12:37:06 -0600

    Se agagrega libreria Spattie permisson para los roles y permisos

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -40,6 +40,7 @@ return array(
     'Symfony\\Component\\CssSelector\\' => array($vendorDir . '/symfony/css-selector'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'Svg\\' => array($vendorDir . '/phenx/php-svg-lib/src/Svg'),
+    'Spatie\\Permission\\' => array($vendorDir . '/spatie/laravel-permission/src'),
     'Ramsey\\Uuid\\' => array($vendorDir . '/ramsey/uuid/src'),
     'Ramsey\\Collection\\' => array($vendorDir . '/ramsey/collection/src'),
     'Psy\\' => array($vendorDir . '/psy/psysh/src'),
