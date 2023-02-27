bba50127e7098ad74e90ea402100f93d79b61e6b
Job Morneo <jobmoreno98@gmail.com>
2023-02-27 11:29:02 -0600

    arreglos 2.0

---
+++ resources/views/pasos_devolucion/pdf.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+        <title>Pasos devolución. SIGE</title>
+    
+        <!-- Styles -->
+    
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+        <link rel="stylesheet" href="{{ asset('css/contrato.css') }}">
+
+        <style>
+            .pie {
+                font-size: 10px;
+                text-align: right;
+            }
+    
+            td {
+                font-size: 12px;
+            }
+
+            .page-break {
+                page-break-after: always;
+            }
+            footer {
+            position: absolute;
+            bottom: 0cm;
+            left: 0cm;
+            right: 0cm;
+            height: 2cm;
+            top: 97%;
+                 
+        }
+          
+            
+        </style>
+
+        
+
+    </head>
+    <body>
+
+        
+        <br>
+
+
+        <center>
+            <div class="position-relative d-flex align-items-center justify-content-center">
+                <h1 class=" text-white" style="-webkit-text-stroke: 1px #dee2e6; font-size: 320%">PASOS DE DEVOLUCIÓN</h1>
+            </div>
+        </center>
+
+
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <table style=" border: 0px" class="table border border-white">
+
+        <tr> 
+            
+            <br><br>
+            <td colspan="1">          
+                <div class="col-lg-2">
+                    <div class="border-left border-primary pt-2 pl-4 ml-2">
+
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 1 -</span> Localizar la documentación del préstamo.</p>
+        
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 2 -</span> Confirmar la identidad de la persona.</p>
+
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 3 -</span> Verificar si es documentación de una persona o de varias, y quien es el último que la tiene.</p>
+
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 4 -</span> Conectar la laptop a la corriente, abrirla, ver que encienda.</p>
+
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 5 -</span> Si presenta algún daño hacer la anotación del mismo.
+                                No tiene repercusiones al usuario.
+                                </p>
+
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 6 -</span> Nos tienen que entregar el cargador. en caso de no traer el cargador o se encuentra dañado hacer la anotación correspondiente.</p>
+
+
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 7 -</span> Una vez revisado el equipo y ver que no presenta daño, ponerlo por escrito, y generar la hoja de préstamo, reimprimirlo con la finalidad de poder nosotros firmarla de recibido a la persona que entrega el equipo. </p>
+                            <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 8 -</span> En caso de haber un daño, se tendrá que hacer una hoja donde el alumno con puño y letra diga cuál fue el daño y el nombre. Hacerle la aclaración que la idea no es cobrarle, si no, para nosotros poderlo reportar a patrimonio con el fin de que el objeto se pueda dar de baja.</p>
+
+           
+                        <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 9 -</span> La hoja de préstamo debe llevar:</p>
+                   
+                                <div class="col-md-12 col-xs-12">
+                                    <ul >
+                                        <li> <p class="letter_size_2 mb-1">Nombre, Firma de quien recibió el equipo, Fecha</p></li>
+                                        <li> <p class="letter_size_2 mb-1">Firma de quien recibió el equipo.</p></li> 
+                                        <li> <p class="letter_size_2 mb-1">Leyenda: Se Recibe Equipo</p></li>
+                                        <li> <p class="letter_size_2 mb-1">Poner el sello de Tecnologías.</p></li>
+                                    </ul>
+                                    <p class="letter_size_2 mb-1"> <b> Nota: </b> Este recibo libera de cualquier otra responsabilidad al alumno.</p>
+                                </div>
+
+                                <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 10 -</span> Imprimir por duplicado, uno se anexa al expediente y el otro se entrega al usuario.</p>
+                                <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 11 -</span> Se le pregunta al usuario si tuvo algún problema con el equipo, algún inconveniente para que sea tomado en cuenta antes de prestarla a alguien más. </p>
+                                <p class="letter_size_2"><span class="font-weight-bold letter_size_2">Paso 12 -</span> El préstamo en SIGE hay que cerrarlo, después de imprimir esto, hay que cambiar el estatus como entregado.</p>
+    
+               
+                            </div>
+                        </div>
+                    
+                </td>
+           
+            </tr>
+            </table>
+        </div>
+    </div>
+   
+          
+
+</body>
+</html>
